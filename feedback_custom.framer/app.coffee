8895649d397188353796ef0939e44081c74a26f6
Elliot Nolten <enolten@bol.com>
2016-02-17 12:47:48 +0100

    feedback prototypes

---
+++ feedback_custom.framer/app.coffee
@@ -0,0 +1,90 @@
+Framer.Device.deviceScale = 1
+# Default cursor
+document.body.style.cursor = "default"
+
+# Import file "feedback"
+sketch = Framer.Importer.load("imported/feedback@1x")
+
+sketch.tooltip.scale = 0.8
+sketch.tooltip.opacity = 0
+
+sketch.tooltip.originX = 0
+sketch.tooltip.originY = 0
+
+sketch.tooltipMsg.opacity = 0
+
+sketch.custom.opacity = 0
+sketch.custom.y = Screen.height - sketch.custom.height + 100
+
+sketch.pilotBtn.y = Screen.height - sketch.pilotBtn.height
+
+mouse = (layer) ->
+	layer.onMouseOver -> this.style.cursor = "pointer"
+	layer.onMouseOut -> this.style.cursor = "default"
+
+mouse(sketch.tooltip)
+mouse(sketch.pilotBtn)
+mouse(sketch.close)
+mouse(sketch.highlight)
+	
+# Standard bounce curve
+bounceCurve = "spring(700,40,10)"
+	
+# The states of the tooltip
+sketch.tooltip.states.add
+	expanded:
+		scale: 1
+		opacity: 1
+	contracted:
+		scale: 0.8
+		opacity: 0
+
+sketch.tooltip.states.animationOptions =
+	curve: bounceCurve
+
+sketch.tooltip.states.switch("expanded")
+
+# The states of the pilotBtn
+sketch.pilotBtn.states.add
+	offCanvas:
+		y: Screen.height
+	onCanvas:
+		y: Screen.height - sketch.pilotBtn.height
+
+sketch.pilotBtn.states.animationOptions =
+	curve: bounceCurve
+
+# The states of the form
+sketch.custom.states.add
+	appear:
+		opacity: 1
+		y: Screen.height - sketch.custom.height
+	disappear:
+		opacity: 0
+		y: Screen.height - sketch.custom.height + 100
+
+sketch.custom.states.animationOptions = 
+	curve: bounceCurve
+
+sketch.tooltip.onClick ->
+	this.states.next()
+	Utils.delay 0.5, ->
+		sketch.pilotBtn.states.switch("offCanvas", curve: "ease-in-out", time: 0.2)
+		Utils.delay 0.1, ->
+			sketch.custom.states.switch("appear")
+
+sketch.close.onClick ->
+	sketch.custom.states.switch("disappear", curve: "ease-in-out", time: 0.2)
+	Utils.delay 0.5, ->
+		sketch.pilotBtn.states.switch("onCanvas")
+
+sketch.pilotBtn.onClick ->
+	sketch.pilotBtn.states.switch("offCanvas")
+	Utils.delay 0.2, ->
+		sketch.tooltip.states.switch("contracted")
+		sketch.custom.states.switch("appear")
+
+sketch.highlight.onClick ->
+	sketch.tooltip.states.switch("expanded")
+
+
---
+++ feedback_custom.framer/imported/feedback@1x/layers.json.js
@@ -0,0 +1,510 @@
+window.__imported__ = window.__imported__ || {};
+window.__imported__["feedback/layers.json.js"] = [
+  {
+    "maskFrame" : null,
+    "id" : "52A1183D-37F2-4DAC-BCEF-AEE097049D8A",
+    "visible" : true,
+    "children" : [
+      {
+        "maskFrame" : null,
+        "id" : "598F23F6-50D9-4F9F-BF4B-F24EDEFB9657",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "7234AB34-5B6B-452A-A05D-FB2E2A54E666",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-close-7234AB34-5B6B-452A-A05D-FB2E2A54E666.png",
+              "frame" : {
+                "y" : 375,
+                "x" : 977,
+                "width" : 8,
+                "height" : 8
+              }
+            },
+            "imageType" : "png",
+            "kind" : "text",
+            "metadata" : {
+              "css" : [
+                "\/* Type something: *\/",
+                "font-family: BolSymbols;",
+                "font-size: 12px;",
+                "color: #000000;",
+                "line-height: 12px;"
+              ],
+              "string" : ""
+            },
+            "layerFrame" : {
+              "y" : 375,
+              "x" : 977,
+              "width" : 8,
+              "height" : 8
+            },
+            "name" : "close"
+          }
+        ],
+        "image" : {
+          "path" : "images\/Layer-custom-598F23F6-50D9-4F9F-BF4B-F24EDEFB9657.png",
+          "frame" : {
+            "y" : 351,
+            "x" : 361,
+            "width" : 650,
+            "height" : 541
+          }
+        },
+        "imageType" : "png",
+        "kind" : "group",
+        "metadata" : {
+          "opacity" : 1
+        },
+        "layerFrame" : {
+          "y" : 351,
+          "x" : 361,
+          "width" : 650,
+          "height" : 541
+        },
+        "name" : "custom"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "D3606723-C456-46CB-B216-55F40236775F",
+        "visible" : false,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "86198FAE-78E9-483D-B7EF-D66C8C152EFE",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-close-86198FAE-78E9-483D-B7EF-D66C8C152EFE.png",
+              "frame" : {
+                "y" : 372,
+                "x" : 975,
+                "width" : 12,
+                "height" : 14
+              }
+            },
+            "imageType" : "png",
+            "kind" : "text",
+            "metadata" : {
+              "css" : [
+                "\/* Type something: *\/",
+                "font-family: BolSymbols;",
+                "font-size: 12px;",
+                "color: #000000;",
+                "line-height: 12px;"
+              ],
+              "string" : ""
+            },
+            "layerFrame" : {
+              "y" : 372,
+              "x" : 975,
+              "width" : 12,
+              "height" : 14
+            },
+            "name" : "close"
+          }
+        ],
+        "image" : {
+          "path" : "images\/Layer-google-D3606723-C456-46CB-B216-55F40236775F.png",
+          "frame" : {
+            "y" : 357,
+            "x" : 560,
+            "width" : 439,
+            "height" : 517
+          }
+        },
+        "imageType" : "png",
+        "kind" : "group",
+        "metadata" : {
+          "opacity" : 1
+        },
+        "layerFrame" : {
+          "y" : 357,
+          "x" : 560,
+          "width" : 439,
+          "height" : 517
+        },
+        "name" : "google"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "EFFE5D13-98C3-44A1-AEDF-2118C19BF4E4",
+        "visible" : true,
+        "image" : {
+          "path" : "images\/Layer-pilotBtn-EFFE5D13-98C3-44A1-AEDF-2118C19BF4E4.png",
+          "frame" : {
+            "y" : 953,
+            "x" : 894,
+            "width" : 97,
+            "height" : 56
+          }
+        },
+        "imageType" : "png",
+        "kind" : "group",
+        "metadata" : {
+          "opacity" : 1
+        },
+        "layerFrame" : {
+          "y" : 953,
+          "x" : 894,
+          "width" : 97,
+          "height" : 56
+        },
+        "name" : "pilotBtn"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "EB4E731A-E945-41D8-A19E-A97CC4C4FC57",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "0E881AB0-62E7-4F08-9479-291B9EF50C54",
+            "visible" : false,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-tooltipMsg-0E881AB0-62E7-4F08-9479-291B9EF50C54.png",
+              "frame" : {
+                "y" : 330,
+                "x" : 466,
+                "width" : 238,
+                "height" : 141
+              }
+            },
+            "imageType" : "png",
+            "kind" : "group",
+            "metadata" : {
+              "opacity" : 1
+            },
+            "layerFrame" : {
+              "y" : 330,
+              "x" : 466,
+              "width" : 238,
+              "height" : 141
+            },
+            "name" : "tooltipMsg"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "9A2D08AC-1082-47C3-B4EE-1CBEF66695B5",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-btn-9A2D08AC-1082-47C3-B4EE-1CBEF66695B5.png",
+              "frame" : {
+                "y" : 472,
+                "x" : 481,
+                "width" : 112,
+                "height" : 30
+              }
+            },
+            "imageType" : "png",
+            "kind" : "group",
+            "metadata" : {
+              "opacity" : 1
+            },
+            "layerFrame" : {
+              "y" : 472,
+              "x" : 481,
+              "width" : 112,
+              "height" : 30
+            },
+            "name" : "btn"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "2E1FF867-1354-40EE-A59C-46317A8097BE",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-tooltipClose-2E1FF867-1354-40EE-A59C-46317A8097BE.png",
+              "frame" : {
+                "y" : 338,
+                "x" : 693,
+                "width" : 8,
+                "height" : 8
+              }
+            },
+            "imageType" : "png",
+            "kind" : "text",
+            "metadata" : {
+              "css" : [
+                "\/* Type something: *\/",
+                "font-family: BolSymbols;",
+                "font-size: 12px;",
+                "color: #000000;",
+                "line-height: 12px;"
+              ],
+              "string" : ""
+            },
+            "layerFrame" : {
+              "y" : 338,
+              "x" : 693,
+              "width" : 8,
+              "height" : 8
+            },
+            "name" : "tooltipClose"
+          }
+        ],
+        "image" : {
+          "path" : "images\/Layer-tooltip-EB4E731A-E945-41D8-A19E-A97CC4C4FC57.png",
+          "frame" : {
+            "y" : 316,
+            "x" : 453,
+            "width" : 268,
+            "height" : 218
+          }
+        },
+        "imageType" : "png",
+        "kind" : "group",
+        "metadata" : {
+          "opacity" : 1
+        },
+        "layerFrame" : {
+          "y" : 316,
+          "x" : 453,
+          "width" : 268,
+          "height" : 218
+        },
+        "name" : "tooltip"
+      },
+      {
+        "maskFrame" : null,
+        "id" : "7B0A6B3D-94CE-4902-A091-18677237A951",
+        "visible" : true,
+        "image" : {
+          "path" : "images\/Layer-highlight-7B0A6B3D-94CE-4902-A091-18677237A951.png",
+          "frame" : {
+            "y" : 308,
+            "x" : 479,
+            "width" : 9,
+            "height" : 9
+          }
+        },
+        "imageType" : "png",
+        "kind" : "group",
+        "metadata" : {
+          "opacity" : 1
+        },
+        "layerFrame" : {
+          "y" : 308,
+          "x" : 479,
+          "width" : 9,
+          "height" : 9
+        },
+        "name" : "highlight"
+      },
+      {
+        "maskFrame" : {
+          "y" : 596,
+          "x" : 44,
+          "width" : 936,
+          "height" : 382
+        },
+        "id" : "8366276F-75B9-4709-9BDA-076B71258502",
+        "visible" : true,
+        "children" : [
+          {
+            "maskFrame" : null,
+            "id" : "CC62B94F-AA55-4438-9D08-B29B99B32389",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-Bekijk_op_bol.com__external-CC62B94F-AA55-4438-9D08-B29B99B32389.png",
+              "frame" : {
+                "y" : 217,
+                "x" : 856,
+                "width" : 124,
+                "height" : 18
+              }
+            },
+            "imageType" : "png",
+            "kind" : "group",
+            "metadata" : {
+              "opacity" : 1
+            },
+            "layerFrame" : {
+              "y" : 217,
+              "x" : 856,
+              "width" : 124,
+              "height" : 18
+            },
+            "name" : "Bekijk_op_bol.com__external"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "2B03CBF7-F861-436E-920E-C8F57122178F",
+            "visible" : false,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-alertInstruction-2B03CBF7-F861-436E-920E-C8F57122178F.png",
+              "frame" : {
+                "y" : 469,
+                "x" : 26,
+                "width" : 972,
+                "height" : 43
+              }
+            },
+            "imageType" : "png",
+            "kind" : "group",
+            "metadata" : {
+              "opacity" : 1
+            },
+            "layerFrame" : {
+              "y" : 469,
+              "x" : 26,
+              "width" : 972,
+              "height" : 43
+            },
+            "name" : "alertInstruction"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "746FF6EB-D1F2-464B-9132-206220CC89BA",
+            "visible" : true,
+            "children" : [
+
+            ],
+            "image" : {
+              "path" : "images\/Layer-back-746FF6EB-D1F2-464B-9132-206220CC89BA.png",
+              "frame" : {
+                "y" : 128,
+                "x" : 26,
+                "width" : 105,
+                "height" : 25
+              }
+            },
+            "imageType" : "png",
+            "kind" : "group",
+            "metadata" : {
+              "opacity" : 1
+            },
+            "layerFrame" : {
+              "y" : 128,
+              "x" : 26,
+              "width" : 105,
+              "height" : 25
+            },
+            "name" : "back"
+          },
+          {
+            "maskFrame" : null,
+            "id" : "BDD08B82-0FB5-4C63-A935-20A982E33510",
+            "visible" : true,
+            "children" : [
+              {
+                "maskFrame" : null,
+                "id" : "0F995EED-2C0B-45D2-8E54-E85771521B35",
+                "visible" : true,
+                "children" : [
+
+                ],
+                "image" : {
+                  "path" : "images\/Layer-headerProfilePrivate-0F995EED-2C0B-45D2-8E54-E85771521B35.png",
+                  "frame" : {
+                    "y" : 54,
+                    "x" : 766,
+                    "width" : 116,
+                    "height" : 17
+                  }
+                },
+                "imageType" : "png",
+                "kind" : "group",
+                "metadata" : {
+                  "opacity" : 1
+                },
+                "layerFrame" : {
+                  "y" : 54,
+                  "x" : 766,
+                  "width" : 116,
+                  "height" : 17
+                },
+                "name" : "headerProfilePrivate"
+              }
+            ],
+            "image" : {
+              "path" : "images\/Layer-sddHeader-BDD08B82-0FB5-4C63-A935-20A982E33510.png",
+              "frame" : {
+                "y" : 29,
+                "x" : 26,
+                "width" : 972,
+                "height" : 85
+              }
+            },
+            "imageType" : "png",
+            "kind" : "group",
+            "metadata" : {
+              "opacity" : 1
+            },
+            "layerFrame" : {
+              "y" : 29,
+              "x" : 26,
+              "width" : 972,
+              "height" : 85
+            },
+            "name" : "sddHeader"
+          }
+        ],
+        "image" : {
+          "path" : "images\/Layer-enrichguioo-8366276F-75B9-4709-9BDA-076B71258502.png",
+          "frame" : {
+            "y" : 29,
+            "x" : 26,
+            "width" : 972,
+            "height" : 949
+          }
+        },
+        "imageType" : "png",
+        "kind" : "group",
+        "metadata" : {
+          "opacity" : 1
+        },
+        "layerFrame" : {
+          "y" : 29,
+          "x" : 26,
+          "width" : 972,
+          "height" : 949
+        },
+        "name" : "enrichguioo"
+      }
+    ],
+    "image" : {
+      "path" : "images\/Layer-enrichment-52A1183D-37F2-4DAC-BCEF-AEE097049D8A.png",
+      "frame" : {
+        "y" : 13,
+        "x" : 201,
+        "width" : 1024,
+        "height" : 1024
+      }
+    },
+    "imageType" : "png",
+    "kind" : "artboard",
+    "metadata" : {
+
+    },
+    "layerFrame" : {
+      "y" : 13,
+      "x" : 201,
+      "width" : 1024,
+      "height" : 1024
+    },
+    "name" : "enrichment"
+  }
+]
