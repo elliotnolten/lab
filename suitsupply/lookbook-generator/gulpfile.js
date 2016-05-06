a66e166ff4aa4559ad758c09195a8cc16de783f3
Elliot Nolten <elliotnolten@gmail.com>
2016-05-06 12:16:34 +0200

    created concatenate gulp task

--- suitsupply/lookbook-generator/gulpfile.js
+++ suitsupply/lookbook-generator/gulpfile.js
@@ -1,5 +1,7 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+var gulp = require('gulp'),
+    browserSync = require('browser-sync').create();
+    concat = require('gulp-concat');
+
 
 gulp.task('browserSync', function() {
   browserSync.init({
@@ -7,6 +9,17 @@ gulp.task('browserSync', function() {
       baseDir: ''
     },
   });
+});
+
+gulp.task('js-concat', function() {
+  return gulp.src([
+    'app/js/jquery.blendmode.js',
+    'node_modules/dropzone/dist/min/dropzone.min.js',
+    'node_modules/html2canvas/dist/html2canvas.min.js',
+    'node_modules/downloadjs/download.min.js'
+  ])
+  .pipe(concat('concat.js'))
+  .pipe(gulp.dest('app/js'));
 });
 
 gulp.task('watch', ['browserSync'], function() {
