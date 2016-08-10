eaa92f34fbc427d9a2c387c2ccc3f2c8d1e55135
Elliot Nolten <elliotnolten@gmail.com>
2016-08-10 16:46:09 +0200

    device scale

--- suitsupply/us_launch/lp_mob.framer/app.coffee
+++ suitsupply/us_launch/lp_mob.framer/app.coffee
@@ -1,3 +1,4 @@
+Framer.Device.deviceScale = 0.5
 # Import file "20160710_box-office_usa-launch" (sizes and positions are scaled 1:2)
 sketch = Framer.Importer.load("imported/20160710_box-office_usa-launch@2x")
 # >>> Framer Fold >>>
--- suitsupply/us_launch/lp_mob.framer/framer/framer.generated.js
+++ suitsupply/us_launch/lp_mob.framer/framer/framer.generated.js
@@ -438,9 +438,9 @@ window.__imported__["20160710_box-office_usa-launch@2x/layers.json.js"] = [
 	}
 ]
 if (DeviceComponent) {DeviceComponent.Devices["iphone-6-silver"].deviceImageJP2 = false};
-if (window.Framer) {window.Framer.Defaults.DeviceView = {"deviceScale":"fit","selectedHand":"","deviceType":"apple-iphone-6s-silver","contentScale":1,"orientation":0};
+if (window.Framer) {window.Framer.Defaults.DeviceView = {"deviceScale":0.5,"selectedHand":"","deviceType":"apple-iphone-6s-silver","contentScale":1,"orientation":0};
 }
-if (window.Framer) {window.Framer.Defaults.DeviceComponent = {"deviceScale":"fit","selectedHand":"","deviceType":"apple-iphone-6s-silver","contentScale":1,"orientation":0};
+if (window.Framer) {window.Framer.Defaults.DeviceComponent = {"deviceScale":0.5,"selectedHand":"","deviceType":"apple-iphone-6s-silver","contentScale":1,"orientation":0};
 }
 window.FramerStudioInfo = {"deviceImagesUrl":"\/_server\/resources\/DeviceImages","documentTitle":"lp_mob.framer"};
 
